76722b5e5def85c3c686129bb384d4335537e166
Taylor Swinton <taylorswinton1@gmail.com>
2019-01-25 23:33:57 +0000

    Automatically backed up by Learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -6,7 +6,6 @@ describe "#using_push" do
   it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
     countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     next_country = "Niger"
-    updated_array = using_push(countries_in_western_africa, next_country)
     expect(updated_array.last).to eq(next_country)
     end
 end
